b098aa0b711033a0709477ba7c4f2c8b068907a9
SinanCayiroglu <cayiroglusinan@live.com>
2024-04-20 14:06:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
 
-const canvas = document.getElementsByClassName('Matrix')[0];
-const context = canvas.getContext('2d');
+// Check if dark mode preference is saved in local storage
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Apply dark mode if it's enabled
+if (isDarkMode) {
+  document.body.classList.add('dark-mode');
+  darkModeToggle.checked = true;
+}
+
+// Toggle dark mode on button click
+darkModeToggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  localStorage.setItem('darkMode', isDarkMode);
+});
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+};
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+};
+
+const canvas = document.getElementsByClassName("Matrix")[0];
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+const katakana =
+  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0123456789";
 
 const alphabet = katakana + latin + nums;
 
@@ -21,11 +50,11 @@ for (let x = 0; x < columns; x++) {
 }
 
 const draw = () => {
-  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = '#0F0';
-  context.font = fontSize + 'px monospace';
+  context.fillStyle = "#0F0";
+  context.font = fontSize + "px monospace";
 
   for (let i = 0; i < rainDrops.length; i++) {
     const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
